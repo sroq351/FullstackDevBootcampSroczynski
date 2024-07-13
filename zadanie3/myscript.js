b17fd1c9875185333e4c630b6852487334821044
Michal <michal.sroczynski@bayer.com>
2024-07-13 15:46:38 +0200

    zadanie 3

---
+++ zadanie3/myscript.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+    document.getElementById("actionButton").onclick = function(){
+        alert(Number(document.getElementById("inputBox1").value) + Number(document.getElementById("inputBox2").value))
+        
+    }
+}
