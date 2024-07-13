0d007522fc7996e015d846293d956a5bb1d7437a
Michal <michal.sroczynski@bayer.com>
2024-07-13 11:31:04 +0200

    data types

---
+++ myscript.js
@@ -0,0 +1,32 @@
+//numeric - zmienne liczbowe 
+let length= 180
+console.log(length)
+let weight = 85.5
+console.log(weight)
+
+//strings - zmienne tekstowe 
+
+let color= "Yellow"
+console.log (color)
+
+//boolean - true/false
+
+let x = true
+console.log(x)
+let y= false
+console.log(y)
+
+//object
+
+let person = {firstName:"Michal", lastName: "Sroczynski"}
+    console.log (person)
+
+//Array
+
+let cars = ["Saab", "Volvo", "BMW"]
+console.log(cars)
+
+//Date
+
+const date = new Date ("2024-08-31")
+console.log(date)
