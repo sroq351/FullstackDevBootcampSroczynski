136dbbfa091239187a388906c51f9c1c9764c918
Michal <michal.sroczynski@bayer.com>
2024-07-13 13:04:03 +0200

    after loops and ifs

--- myscript.js
+++ myscript.js
@@ -30,3 +30,81 @@ console.log(cars)
 
 const date = new Date ("2024-08-31")
 console.log(date)
+
+
+//equal 
+
+console.log ( "wynik porownaia: " + (length==weight))
+
+//equal value and equal type
+let lenght2 = 180.0
+console.log (length===lenght2)
+
+//not equal 
+console.log (100 !=150)
+
+//not equal value and not  equal type
+
+console.log (100 !== "100")
+
+//greater
+
+console.log (100>99)
+
+//less
+
+console.log(99<100)
+
+//greater or equal
+
+console.log (100 >=100)
+
+//less or equal
+
+console.log (100<=99)
+
+//ternary operator
+
+let result = 100 > 101 ? "Tak" : "Nie"
+
+console.log (result)
+
+// loops w onenocie 
+
+
+
+//for each
+cars.forEach (function(element){
+    console.log (element)
+})
+
+
+
+
+//IF else (+AND)
+let temperature = 41
+if(temperature>= 36. && temperature<37) {
+    console.log ("W normie")
+}else if (temperature>=37 && temperature< 38){
+    console.log ("Stan podgoraczkowy")
+}else if (temperature>=38 && temperature<40) {
+    console.log("goraczka")
+}else{
+    console.log("Blad pomiaru")
+}
+//or 
+let money =25
+let voucher = true
+
+if (money> 20 || voucher== true) {
+    console.log("Mozesz isc na film")
+}
+
+//not
+
+if (!15>20){
+    console.log ("NOT")
+
+}
+
+funciton
