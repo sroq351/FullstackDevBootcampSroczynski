2b6eb4db0d19f1d14a5bcf03f9e890549e5719e0
Michal <michal.sroczynski@bayer.com>
2024-07-14 14:10:35 +0200

    zadanie1_14072024

---
+++ zadanie1_14072024/myscript.js
@@ -0,0 +1,10 @@
+const Offer =function(brand,model,prize){
+    this.brand=brand
+    this.model=model
+    this.prize=prize
+}
+Offer.prototype.cost100km =() => console.log (37)
+
+const offer1 = new Offcer ("Opel", "Kadett", "2000")
+
+offer1.cost100km()
