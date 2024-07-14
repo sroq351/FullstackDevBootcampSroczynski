dff3e579b2940bd4e0cf74b9196aa683ee6cefaa
Michal <michal.sroczynski@bayer.com>
2024-07-14 13:17:06 +0200

    persons (class and instances

--- myscript.js
+++ myscript.js
@@ -1,44 +1,14 @@
-function Click() {
-    this.handlers = []
+const Person = function(name, surname, age) {
+this.name = name
+this.surname = surname
+this.age = age
 }
 
-Click.prototype = {
 
-    subscribe: function (fn) {
-        this.handlers.push(fn)
-    },
+const person = new Person ("Jan", "Kowalski", 18)
+const person2 = new Person ("Jan", "Nowak", 20) 
+console.log(person.name)
+console.log(person2)
 
-    unsubscribe: function (fn) {
-        this.handlers = this.handlers.filter(
-            function (item) {
-                if (item !== fn) {
-                    return item;
-                }
-            }
-        )
-    },
-    fire: function (o, thisObj) {
-        var scope = thisObj || window
-        this.handlers.forEach(function (item) {
-            item.call(scope, o)
-        })
-    }
-}
-function run() {
-    var clickHandler = function (item) {
-        console.log("Fired: " + item)
-    };
-
-    var click = new Click()
-
-    click.subscribe(clickHandler);
-    click.fire("event #1");
-    click.unsubscribe(clickHandler);
-    click.fire("event #2");
-    click.subscribe(clickHandler);
-    click.fire("event #3");
-}
-
-window.onload = function () {
-    run()
-}
+let persons = [person,person2]
+console.log(persons)
